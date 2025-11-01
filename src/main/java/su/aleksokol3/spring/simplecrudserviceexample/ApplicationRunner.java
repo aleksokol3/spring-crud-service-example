ea53cb9b41d69e663a8c3bd2b1fb5011677fe120
Aleksey Sokolov <aleksokol3@yandex.ru>
2025-11-01 12:56:59 +0300

    feat: init commit

---
+++ src/main/java/su/aleksokol3/spring/simplecrudserviceexample/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package su.aleksokol3.spring.simplecrudserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+}
